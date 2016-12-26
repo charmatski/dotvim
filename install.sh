b3e878954605fc81b4b2f956599707e18bd1a419
charmatski <charmatski@users.noreply.github.com>
2016-12-25 22:24:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 git clone git://github.com/charmatski/dotvim/ ~/.vim
-git clone git://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone git://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 vim +BundleInstall +qall
